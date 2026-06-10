0e10c501366a2348f622d4908a72bc20362da3bc
Patrick Butler <pbutler@nebius.com>
2026-06-10 11:43:32 -0400

    fix(provider): updated all references to new public nebius providuer
    This changes all the old references to the nebius provider in our
    private repo to a new one in the public repos.  I left fixing version
    constraints to another patch where ever possible.

--- k8s-training/provider.tf
+++ k8s-training/provider.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     nebius = {
-      source  = "terraform-provider.storage.eu-north1.nebius.cloud/nebius/nebius"
-      version = ">= 0.5.232"
+      source  = "nebius/nebius"
+      version = ">= 0.6.23"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
