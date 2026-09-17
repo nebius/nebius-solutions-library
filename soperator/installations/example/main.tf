b8fcfc98659a423febee6dc9c27814fd58f0e1b0
Dmitry Starov <dmitry.staroff@gmail.com>
2026-09-17 16:37:52 +0200

    [ADD] Check for resource preset sufficiency to attach WEKA before creating filesystems

--- soperator/installations/example/main.tf
+++ soperator/installations/example/main.tf
@@ -231,6 +231,7 @@ resource "terraform_data" "check_variables" {
     terraform_data.check_nfs_sustainability,
     terraform_data.check_jail_submount_paths,
     terraform_data.check_weka_count,
+    terraform_data.check_resource_presets_for_weka,
     terraform_data.check_local_nvme,
   ]
 }
